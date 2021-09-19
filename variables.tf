f4913923501434ebeb7a2f05faa0b0bf00c92d75
Kajal Sharma <34881990+sharmakajal@users.noreply.github.com>
2021-09-19 20:02:49 +0530

    Added script file

---
+++ variables.tf
@@ -0,0 +1,14 @@
+
+variable "resource_group_name" {
+  default = "prod_resource"
+}
+
+variable "resource_group_location" {
+  default = "Central India"
+}
+variable "virtual_network_name" {
+  default = "prod_vnet"
+}
+variable "subnet" {
+  default = "myTFsubnet"
+}
